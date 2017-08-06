a1ce69caf418aad121a693678d495319752a12e4
velopert <public.velopert@gmail.com>
2017-08-06 14:47:47 +0900

    데이터 수정 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -44,17 +44,41 @@ class App extends Component {
     });
   }
 
+  // 투두아이템 토글하기
+  handleToggle = (id) => {
+    // id 로 배열의 인덱스를 찾습니다.
+    const { todos } = this.state;
+    const index = todos.findIndex(todo => todo.id === id);
+    
+    // 찾은 데이터의 done 값을 반전시킵니다.
+    const toggled = {
+      ...todos[index],
+      done: !todos[index].done
+    };
+
+    // slice 를 통하여 우리가 찾은 index 전 후의 데이터들을 복사합니다
+    // 그리고 그 사이에는 변경된 투두 객체를 넣어줍니다.
+    this.setState({
+      todos: [
+        ...todos.slice(0, index),
+        toggled,
+        ...todos.slice(index + 1, todos.length)
+      ]
+    });
+  }
+
   render() {
     const { input, todos } = this.state;
     const { 
       handleChange,
-      handleInsert
+      handleInsert,
+      handleToggle
     } = this;
 
     return (
       <PageTemplate>
         <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} onToggle={handleToggle}/>
       </PageTemplate>
     );
   }
